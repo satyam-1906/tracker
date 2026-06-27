122f37ddf82cd564d214766c2dac616b75461b4c
satyam-1906 <scientificsatyam1906@gmail.com>
2026-06-28 03:32:24 +0530

    added a debug feature

--- webhook.py
+++ webhook.py
@@ -1,34 +1,24 @@
 from fastapi import FastAPI, Request, HTTPException
 import database
 import threading
-from pydantic import BaseModel
 
 app = FastAPI()
 data = []
-
-class DataSchema(BaseModel):
-    device_id: str
-    latitude: float
-    longitude: float
-    accuracy: float
-    speed: float 
-    heading: int
-    timestamp: int
-
 
 def calling():
     global data
     if len(data) > 0:
         database.put_data(data)
-    call_timer = threading.Timer(10.0, calling)
+    call_timer = threading.Timer(5.0, calling)
     call_timer.start()
     data = []
 
 
 @app.post("/webhook")
-async def handle_webhook(payload: DataSchema):
+async def handle_webhook(request: Request):
     global data
     try:
+        payload = await request.json()
         print(f"Received Webhook Payload: {payload}")
         data.append(payload)
         
