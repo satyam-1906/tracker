20713b1106fc45f5693875d051029e039a0b66b6
satyam-1906 <scientificsatyam1906@gmail.com>
2026-06-28 14:33:21 +0530

    update marker API endpoint added

--- webhook.py
+++ webhook.py
@@ -1,34 +1,25 @@
 from fastapi import FastAPI, Request, HTTPException
 import database
 import threading
-from pydantic import BaseModel
 
 app = FastAPI()
 data = []
-
-class DataSchema(BaseModel):
-    device_id: str
-    latitude: float
-    longitude: float
-    accuracy: float
-    speed: float 
-    heading: int
-    timestamp: int
-
 
 def calling():
     global data
     if len(data) > 0:
         database.put_data(data)
-    call_timer = threading.Timer(10.0, calling)
+    call_timer = threading.Timer(5.0, calling)
     call_timer.start()
     data = []
 
+calling()
 
 @app.post("/webhook")
-async def handle_webhook(payload: DataSchema):
-    global data
+async def handle_webhook(request: Request):
+    global data, payload
     try:
+        payload = await request.json()
         print(f"Received Webhook Payload: {payload}")
         data.append(payload)
         
@@ -36,5 +27,6 @@ async def handle_webhook(payload: DataSchema):
     except Exception as e:
         raise HTTPException(status_code=400, detail="Invalid JSON payload")
     
-
-calling()
+app.get('/getMarker')
+async def sendMarker():
+    return payload
