97689b8a570b08cecca7d788a4778fdf995ef507
satyam-1906 <scientificsatyam1906@gmail.com>
2026-06-30 21:41:39 +0530

    debugging

--- webhook.py
+++ webhook.py
@@ -47,8 +47,8 @@ async def warn(payload):
             my_location = database.my_location()
             curr_dist = haversine(float(my_location[0]['last_coords'][0]), float(my_location[0]['last_coords'][1]), float(payload['latitude']), float(payload['longitude']))
             print(curr_dist)
-            if curr_dist <= alarm['distance']:
-                response = requests.get('https://api.callmebot.com/text.php?user=@asf1906&text=This+is+a+test+message')
+            #if curr_dist <= alarm['distance']:
+                #response = requests.get('https://api.callmebot.com/text.php?user=@asf1906&text=This+is+a+test+message')
 
 
 @app.get("/")
